b7cc243802af0b376142a40290fda4bf03e1e322
Efebey2903 <swatibaljeet0101@gmail.com>
2024-10-11 21:20:15 +0300

    Create win.sh

---
+++ win.sh
@@ -0,0 +1,1 @@
+sudo wget --no-check-certificate -qO InstallNET.sh 'https://raw.githubusercontent.com/kmille36/CaiWindowsChoLinux/main/InstallNET.sh' && bash InstallNET.sh -dd 'https://bit.ly/3ITXLJ9'
